7f1f9ad296b0979465611bad825487113d638b50
xdavidwu <xdavidwuph@gmail.com>
2019-03-31 22:35:14 +0800

    introduce linux_distro_to_ua and append arch in windows_nt_version_to_ua

--- rndua/format.py
+++ rndua/format.py
@@ -10,5 +10,8 @@ class Formatter:
     def os_x_version_to_chrome(self,version):
         return 'Macintosh; Intel Mac OS X '+version.replace('.','_')
 
-    def windows_nt_version_to_ua(self,version):
-        return 'Windows NT '+version
+    def windows_nt_version_to_ua(self,version,arch=''):
+        return 'Windows NT '+version+arch
+
+    def linux_distro_to_ua(self,distro,arch='x86_64'):
+        return 'X11; '+distro+'Linux '+arch
