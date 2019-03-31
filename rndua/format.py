eee237f4745f60df1103a87f89a1b412fe5c76c8
xdavidwu <xdavidwuph@gmail.com>
2019-03-31 22:50:01 +0800

    introduce android_environment_to_ua

--- rndua/format.py
+++ rndua/format.py
@@ -15,3 +15,25 @@ class Formatter:
 
     def linux_distro_to_ua(self,distro,arch='x86_64'):
         return 'X11; '+distro+'Linux '+arch
+
+    def android_environment_to_ua(self,env,lang='',firefox=False):
+        res='Linux; '
+        if lang!='': # old Android Browser
+            res+='U; '
+        res+='Android '
+        if env[1]=='9.0.0':
+            res+='9'
+        elif env[1].startswith('8'):
+            res+=env[1]
+        elif env[1].endswith('.0'):
+            res+=env[1][:-2]
+        else:
+            res+=env[1]
+        res+='; '
+        if lang!='':
+            res+=lang+'; '
+        elif firefox:
+            res+='Mobile'
+            return res
+        res+=env[2]+' Build/'+env[0]
+        return res
