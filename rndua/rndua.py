a11e7d189939c485c4e9de2d7669ae37e22f6e7e
xdavidwu <xdavidwuph@gmail.com>
2019-04-01 22:27:57 +0800

    introduce get_random_linux_{chrome,firefox}_ua and get_random_ua

--- rndua/rndua.py
+++ rndua/rndua.py
@@ -65,4 +65,35 @@ class RandomUAGenerator():
                     self.eg.get_random_os_x_version(
                         minimum=os_x_minimum,maximum=os_x_maximum),),
                     self.bg.get_random_firefox_version(
-                        minimum=firefox_minimum,maximum=firefox_maximum),)
+                        minimum=firefox_minimum,maximum=firefox_maximum))
+
+    def get_random_linux_chrome_ua(self,chrome_minimum=60,
+            chrome_maximum=74):
+        return self.fm.format_chrome_ua('X11; Linux x86_64',
+                self.bg.get_random_chrome_version(minimum=chrome_minimum,
+                    maximum=chrome_maximum))
+
+    def get_random_linux_firefox_ua(self,firefox_minimum=50,
+            firefox_maximum=66,os_x_minimum='10.4.4',os_x_maximum='10.14.4'):
+        return self.fm.format_firefox_ua(''.join(['X11; ',
+            self.eg.get_random_linux_distro_firefox_sig(),'Linux x86_64']),
+            self.bg.get_random_firefox_version(
+                minimum=firefox_minimum,maximum=firefox_maximum))
+
+    def get_random_ua(self):
+        val=random.randint(0,100)
+        if val>=95:
+            return self.get_random_os_x_chrome_ua()
+        if val>=90:
+            return self.get_random_os_x_firefox_ua()
+        if val>=85:
+            return self.get_random_os_x_chrome_ua()
+        if val>=70:
+            return self.get_random_os_x_firefox_ua()
+        if val>=50:
+            return self.get_random_windows_chrome_ua()
+        if val>=40:
+            return self.get_random_windows_firefox_ua()
+        if val>=5:
+            return self.get_random_android_chrome_ua()
+        return self.get_random_android_firefox_ua()
